d061cd4f8d7b67bc01d2b82b6d9d3d9194ea4c2a
jaharul007 <hfudgdhdhhsus@gmail.com>
2026-02-03 11:25:13 +0530

    Create models/User.js

---
+++ models/User.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    phone: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    inviteCode: { type: String },
+    balance: { type: Number, default: 0 },
+    createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.models.User || mongoose.model('User', UserSchema);
