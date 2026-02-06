6c08a3e508f99f55578e0e73bb983aae1d3e8569
jaharul007 <hfudgdhdhhsus@gmail.com>
2026-02-06 11:41:18 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,11 +1,16 @@
 import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
-    phoneNumber: { type: String, required: true, unique: true },
+    // Field ka naam 'phoneNumber' hi rakhein jo aapke index.html se aa raha hai
+    phoneNumber: { 
+        type: String, 
+        required: true, 
+        unique: true,
+        trim: true 
+    },
     password: { type: String, required: true },
     inviteCode: { type: String, default: "" },
     balance: { type: Number, default: 0 }
 });
 
-// Vercel ke liye ye line bahut zaroori hai
 export default mongoose.models.User || mongoose.model('User', userSchema);
