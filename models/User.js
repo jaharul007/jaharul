cb3e584efb4892044015e6cdc6181c1c0a15bae9
jaharul007 <hfudgdhdhhsus@gmail.com>
2026-02-07 12:40:11 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,16 +1,27 @@
 import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
-    // Field ka naam 'phoneNumber' hi rakhein jo aapke index.html se aa raha hai
     phoneNumber: { 
         type: String, 
         required: true, 
-        unique: true,
-        trim: true 
+        unique: true 
     },
-    password: { type: String, required: true },
-    inviteCode: { type: String, default: "" },
-    balance: { type: Number, default: 0 }
-});
+    password: { 
+        type: String, 
+        required: true 
+    },
+    balance: { 
+        type: Number, 
+        default: 1000 
+    },
+    inviteCode: { 
+        type: String, 
+        default: "" 
+    },
+    createdAt: { 
+        type: Date, 
+        default: Date.now 
+    }
+}, { collection: 'users' });
 
 export default mongoose.models.User || mongoose.model('User', userSchema);
